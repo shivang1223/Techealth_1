199fb556cccaf175420aa0e0f40aba1ce5e266a6
Shivang Tiwari <shivangtiwari2312@gmail.com>
2025-01-22 13:52:55 +0530

    commit

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -1,13 +1,16 @@
 
 import connectDB from "@/lib/db";
+import Contact from "@/model/contact.model";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
     try {
-        
         await connectDB()
-
-        return NextResponse.json({ message: "Database connected " }, { status: 201 })
+        const {email, name, message} = await request.json()
+        await Contact.create({
+            email, name, message
+        })
+        return NextResponse.json({ message: "Message sent" }, { status: 201 })
     } catch (error) {
         return NextResponse.json({ error: error.message }, { status: 503 })
     }
